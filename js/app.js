1ffcc6837bc67e0e0631e03cac242a684e9312d2
Eboweme Ayo <ebowemeayo@yahoo.com>
2023-06-14 13:33:14 +0100

    updated toggle menu

--- js/app.js
+++ js/app.js
@@ -14,36 +14,11 @@ const menuToggle = document.getElementById('menuToggle');
 const headerMenuMobile = document.querySelector('.headerMenuMobile');
 
 // Add click event listener to the image element
-// menuToggle.addEventListener('click', function () {
 menuToggle.addEventListener('click', () => {
    
     // console.log('Click event triggered');
     headerMenuMobile.classList.toggle('visible');
     menuToggle.setAttribute('aria-expanded', true);
-
-    
-    // const isVisible = headerMenuMobile.getAttribute('data-visible') === 'true';
-
-    // if (isVisible) {
-    //   headerMenuMobile.setAttribute('data-visible', 'false');
-    // } else {
-    //   headerMenuMobile.setAttribute('data-visible', 'true');
-    // }
 });
-//     const visibility = headerMenuMobile.getAttribute("data-visible");
-    
-//     if (visibility === "false") {
-//         headerMenuMobile.setAttribute('data-visible', 'true');
-//     }
-//     else {
-//         headerMenuMobile.setAttribute('data-visible', 'false');
-//     }
-// });
-//   // Toggle the visibility of the menu by modifying the display property
-//   if (menu.style.display === 'block') {
-//     menu.style.display = 'none';
-//   } else {
-//     menu.style.display = 'block';
-//   }
-// });
+
 
